ce968a468a51f22a8e5c80f2cf7005fc52706163
Marcos Jesus <marcos.jesus@gomara.tech>
2022-08-29 18:51:16 -0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class frmExecutando
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmExecutando));
+			this.pctGif = new System.Windows.Forms.PictureBox();
+			this.btnParar = new System.Windows.Forms.Button();
+			this.btnInicio = new System.Windows.Forms.Button();
+			this.lblConfiguracao = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pctGif)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pctGif
+			// 
+			this.pctGif.Image = ((System.Drawing.Image)(resources.GetObject("pctGif.Image")));
+			this.pctGif.Location = new System.Drawing.Point(12, 12);
+			this.pctGif.Name = "pctGif";
+			this.pctGif.Size = new System.Drawing.Size(312, 303);
+			this.pctGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pctGif.TabIndex = 0;
+			this.pctGif.TabStop = false;
+			// 
+			// btnParar
+			// 
+			this.btnParar.Location = new System.Drawing.Point(168, 323);
+			this.btnParar.Name = "btnParar";
+			this.btnParar.Size = new System.Drawing.Size(75, 23);
+			this.btnParar.TabIndex = 1;
+			this.btnParar.Text = "&Parar";
+			this.btnParar.UseVisualStyleBackColor = true;
+			this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+			// 
+			// btnInicio
+			// 
+			this.btnInicio.Location = new System.Drawing.Point(249, 323);
+			this.btnInicio.Name = "btnInicio";
+			this.btnInicio.Size = new System.Drawing.Size(75, 23);
+			this.btnInicio.TabIndex = 2;
+			this.btnInicio.Text = "Início";
+			this.btnInicio.UseVisualStyleBackColor = true;
+			this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+			// 
+			// lblConfiguracao
+			// 
+			this.lblConfiguracao.AutoSize = true;
+			this.lblConfiguracao.Location = new System.Drawing.Point(12, 328);
+			this.lblConfiguracao.Name = "lblConfiguracao";
+			this.lblConfiguracao.Size = new System.Drawing.Size(0, 13);
+			this.lblConfiguracao.TabIndex = 3;
+			// 
+			// frmExecutando
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(336, 356);
+			this.Controls.Add(this.lblConfiguracao);
+			this.Controls.Add(this.btnInicio);
+			this.Controls.Add(this.btnParar);
+			this.Controls.Add(this.pctGif);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(352, 395);
+			this.MinimumSize = new System.Drawing.Size(352, 395);
+			this.Name = "frmExecutando";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "App Move...";
+			this.Load += new System.EventHandler(this.frm_Executando_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pctGif)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pctGif;
+		private System.Windows.Forms.Button btnParar;
+		private System.Windows.Forms.Button btnInicio;
+		private System.Windows.Forms.Label lblConfiguracao;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class frmExecutando : Form
+	{
+		Thread thread;
+
+		[DllImport("user32.dll")]
+		public static extern bool SetCursorPos(int x, int y);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+		public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint CButtons, uint dwExtraInfo);
+		private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+		private const int MOUSEEVENTF_LEFT_UP = 0x04;
+		private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+		private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+		[DllImport("user32.dll")]
+		public static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct DEVMODE
+		{
+			private const int CCHDEVICENAME = 0x20;
+			private const int CCHFORMNAME = 0x20;
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+			public string dmDeviceName;
+			public short dmSpecVersion;
+			public short dmDriverVersion;
+			public short dmSize;
+			public short dmDriverExtra;
+			public int dmFields;
+			public int dmPositionX;
+			public int dmPositionY;
+			public ScreenOrientation dmDisplayOrientation;
+			public int dmDisplayFixedOutput;
+			public short dmColor;
+			public short dmDuplex;
+			public short dmYResolution;
+			public short dmTTOption;
+			public short dmCollate;
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+			public string dmFormName;
+			public short dmLogPixels;
+			public int dmBitsPerPel;
+			public int dmPelsWidth;
+			public int dmPelsHeight;
+			public int dmDisplayFlags;
+			public int dmDisplayFrequency;
+			public int dmICMMethod;
+			public int dmICMIntent;
+			public int dmMediaType;
+			public int dmDitherType;
+			public int dmReserved1;
+			public int dmReserved2;
+			public int dmPanningWidth;
+			public int dmPanningHeight;
+		}
+
+		public new int Width { get; set; }
+
+		public new int Height { get; set; }
+
+		public frmExecutando()
+		{
+			InitializeComponent();
+		}
+
+		protected override void WndProc(ref Message message)
+		{
+			const int WM_SYSCOMMAND = 0x0112;
+			const int SC_MOVE = 0xF010;
+
+			switch (message.Msg)
+			{
+				case WM_SYSCOMMAND:
+					int command = message.WParam.ToInt32() & 0xfff0;
+					if (command == SC_MOVE)
+						return;
+					break;
+			}
+
+			base.WndProc(ref message);
+		}
+
+		private void frm_Executando_Load(object sender, EventArgs e)
+		{
+			pctGif.Visible = false;
+			CarregarConfDisplay();
+		}
+
+		public void SetCursorPosition(int x, int y)
+		{
+			Cursor.Position = this.PointToScreen(new Point(x, y));
+			SetCursorPos(x, y);
+		}
+
+		public void DoMouseClick()
+		{
+			uint x = (uint)Cursor.Position.X;
+			uint y = (uint)Cursor.Position.Y;
+			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFT_UP, x, y, 0, 0);
+		}
+
+		private void btnInicio_Click(object sender, EventArgs e)
+		{
+			ThreadStart threadStart = new ThreadStart(Execution);
+			thread = new Thread(threadStart);
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+		}
+
+		private void Execution()
+		{
+			Random rdNum = new Random();
+
+			while (true)
+			{
+				btnInicio.Invoke((MethodInvoker)delegate
+				{
+					this.Text = "App Move processando...";
+					pctGif.Visible = true;
+					btnInicio.Text = "Executando";
+					btnInicio.Enabled = false;
+					lblConfiguracao.Text = $"Config. Video: {Width} x {Height}";
+					SetCursorPosition((Width / 2) + rdNum.Next(100), (Height / 2) + rdNum.Next(100));
+					DoMouseClick();
+				});
+
+				Application.DoEvents();
+				Thread.Sleep(10000);
+			}
+		}
+
+		private void CarregarConfDisplay()
+		{
+			DEVMODE vDevMode = new DEVMODE();
+			int i = 0;
+			EnumDisplaySettings(null, i, ref vDevMode);
+			Width = vDevMode.dmPelsWidth;
+			Height = vDevMode.dmPelsHeight;
+		}
+
+		private void btnParar_Click(object sender, EventArgs e)
+		{
+			if (thread != null && (thread.ThreadState == ThreadState.Running ||
+				thread.ThreadState == ThreadState.Background ||
+				thread.ThreadState == ThreadState.WaitSleepJoin))
+			{
+				thread.Abort();
+				pctGif.Visible = false;
+				btnInicio.Enabled = true;
+				this.Text = "App Move...";
+				btnInicio.Text = "Início";
+			}
+			else
+			{
+				MessageBox.Show("O Processo não esta em execução.",
+								"Informação Importante",
+								MessageBoxButtons.OK,
+								MessageBoxIcon.Information
+								);
+			}
+		}
+	}
+}
